cd7bd3822e54fa5b159243c8449be04a78fdb5b8
Søren Løbner <sl@biq.dk>
2015-02-17 11:01:30 +0100

    Added zip and sourcecode for chrome-plugin

---
+++ 0.9.2_0/ordbogen.js
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2013 lobner of lobner.dk
+// Use of this source code is governed by a BSD-style license
+
+
+chrome.contextMenus.create({
+    title:"Ordbogen.com: '%s'",
+    contexts: ["selection"], 
+    "onclick": getClickHandler()
+});
+
+/**
+ * Returns a handler which will open a new window when activated.
+ */
+function getClickHandler() {
+  return function(info, tab) {
+
+    // The srcUrl property is only available for image elements.
+    var url = 'https://www.ordbogen.com/opslag.php?dict=auto&word=' + info.selectionText;
+
+    // Create a new window to the info page.
+    chrome.tabs.create({ url: url, index: tab.index+1 });
+  };
+};
